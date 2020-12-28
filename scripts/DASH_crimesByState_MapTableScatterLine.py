ba08f7419105f70a7e14d1dda237c2cbdcbad977
jreyesox <jreyesox@gmail.com>
2020-12-28 03:10:37 -0500

    20201228 github data pull

--- scripts/DASH_crimesByState_MapTableScatterLine.py
+++ scripts/DASH_crimesByState_MapTableScatterLine.py
@@ -28,17 +28,35 @@ styles = {
     }
 }
 #%%
-# Set Data Directory
-os.chdir(r'C:\Users\jreye\Documents\Projects\FBI-UCR\data\normalized')
-# Pull in Data 
-dfs = []
-for file in glob.glob('*.csv'):
-    df = pd.read_csv(file)
-    df = df[df.columns[1:]].reset_index(drop=True)
-    dfs.append(df)
+def gitData(startYear, endYear):
+    # Downloading the csv file from your GitHub account
+    yyyy = list(range(startYear,endYear+1))
+    gitFolder =\
+    'https://raw.githubusercontent.com/jreyesox/FBI-UCR/main/data/normalized/'
+    allFiles = []
+    for year in yyyy:
+        file = gitFolder+str(year)+'_crimes_byState.csv'
+        #print(file)
+        df = pd.read_csv(file)
+        allFiles.append(df)
+    dff = pd.concat(allFiles)
+    dff = dff.reset_index(drop=True)
+    dff = dff.rename(columns={'Unnamed: 0':'checker'})
+    return dff
+#%%
+crimesAll_df = gitData(2013,2019)
 
-# Concatenate all data into one DataFrame
-crimesAll_df = pd.concat(dfs, ignore_index=True)
+# # Set Data Directory
+# os.chdir(r'C:\\folder\file.csv')
+# # Pull in Data 
+# dfs = []
+# for file in glob.glob('*.csv'):
+#     df = pd.read_csv(file)
+#     df = df[df.columns[1:]].reset_index(drop=True)
+#     dfs.append(df)
+
+# # Concatenate all data into one DataFrame
+# crimesAll_df = pd.concat(dfs, ignore_index=True)
 
 
 #%%
